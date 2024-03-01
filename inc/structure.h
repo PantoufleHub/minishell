e5ef17938e2e7842deee2fa60992fb37b8b823e7
jbidaux <jeremie.bidaux@gmail.com>
2024-03-01 13:21:38 +0100

    mmh changed to s_cmds

--- inc/structure.h
+++ inc/structure.h
@@ -48,10 +48,10 @@ typedef struct s_cmd
 	int			fd_out;
 }	t_cmd;
 
-typedef struct s_cmd_nodes
+typedef struct s_cmds
 {
 	t_cmd				*cmd;
 	struct s_cmd_node	*next;
-}	t_cmd_nodes;
+}	t_cmds;
 
 #endif

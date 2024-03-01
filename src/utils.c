c3dadace3b8785f3e8587137f68ec3c5674bafdd
jbidaux <jeremie.bidaux@gmail.com>
2024-03-01 16:35:01 +0100

    trying to add the new layer of parsing but it's hard

--- src/utils.c
+++ src/utils.c
@@ -19,7 +19,7 @@ void	entry_display(void)
 		"███  █████ █████\n");
 	ft_printf("  ░░░░░     ░░░░░ ░░░░░ ░░░░ ░░░░░ ░░░░░ ░░░░░░  ░░░░ ░░░░░  ░░░"
 		"░░░  ░░░░░ ░░░░░\n"NRM);
-	ft_printf(NRM"  v0.4.1 Do you even care about the ENVIRONMENT? (variables)"
+	ft_printf(BLU"  v0.4.2 Is that you Bilbo ? I wish baggin was that easy"
 		"\n\n"NRM);
 	ft_printf(MAG"                This project is brought to you by jbido and a"
 		"péro\n\n\n"NRM);

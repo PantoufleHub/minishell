e69ffd912651851a96c0a81ebe4de919049762e7
jbidaux <jeremie.bidaux@gmail.com>
2024-03-12 16:58:54 +0100

    starting cd

--- src/cd.c
+++ src/cd.c
@@ -1,2 +1,22 @@
 #include "../inc/minishell.h"
 
+int ft_cd(void)
+{
+    char    *cwd;
+
+    cwd = getcwd(NULL, 0);
+    if (!cwd)
+    {
+        perror("getcwd");
+        return (EXIT_FAILURE);
+    }
+    printf("Current working directory: %s\n", cwd);
+    free(cwd);
+    return (EXIT_SUCCESS);
+}
+
+// int main()
+// {
+//     ft_cd();
+//     return (0);
+// }

5841ea91ecf4808f9485c6df2e7b3161b0fe876b
jbidaux <jeremie.bidaux@gmail.com>
2024-03-13 17:54:31 +0100

    added working cd (hopefully)

--- builtins/cd.c
+++ builtins/cd.c
@@ -1,22 +1,40 @@
 #include "../inc/minishell.h"
 
-int ft_cd(void)
-{
-    char    *cwd;
-
-    cwd = getcwd(NULL, 0);
-    if (!cwd)
-    {
-        perror("getcwd");
-        return (EXIT_FAILURE);
-    }
-    printf("Current working directory: %s\n", cwd);
-    free(cwd);
-    return (EXIT_SUCCESS);
-}
-
-// int main()
+// int ft_cd(char **av)
 // {
-//     ft_cd();
-//     return (0);
+//     char    *cwd;
+//     char    *newd;
+
+//     cwd = getcwd(NULL, 0);
+//     if (!cwd)
+//     {
+//         perror("getcwd");
+//         return (EXIT_FAILURE);
+//     }
+//     printf("Current working directory: %s\n", cwd);
+//     newd = av[1];
+//     if (chdir(newd) == -1)
+//     {
+//         perror("chdir failed");
+//         return (EXIT_FAILURE);       
+//     }
+//     cwd = getcwd(NULL, 0);
+//     if (!cwd)
+//     {
+//         perror("getcwd");
+//         return (EXIT_FAILURE);
+//     }
+//     printf("Current working directory: %s\n", cwd);   
+//     free(cwd);
+//     return (EXIT_SUCCESS);
 // }
+
+int main(int ac, char **av)
+{
+    if (chdir(av[1]) == -1)
+    {
+        perror("chdir failed");
+        return (EXIT_FAILURE);       
+    }
+    return (0);
+}

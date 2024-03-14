cdc33cf83cd06766cb3c24aef3883ad53b97613e
jbidaux <jeremie.bidaux@gmail.com>
2024-03-14 10:24:29 +0100

    cd synthax change to be supported without execve

--- builtins/cd.c
+++ builtins/cd.c
@@ -1,41 +1,49 @@
 #include "../inc/minishell.h"
 
-// int ft_cd(char **av)
-// {
-//     char    *cwd;
-//     char    *newd;
-
-//     cwd = getcwd(NULL, 0);
-//     if (!cwd)
-//     {
-//         perror("getcwd");
-//         return (EXIT_FAILURE);
-//     }
-//     printf("Current working directory: %s\n", cwd);
-//     newd = av[1];
-//     if (chdir(newd) == -1)
-//     {
-//         perror("chdir failed");
-//         return (EXIT_FAILURE);       
-//     }
-//     cwd = getcwd(NULL, 0);
-//     if (!cwd)
-//     {
-//         perror("getcwd");
-//         return (EXIT_FAILURE);
-//     }
-//     printf("Current working directory: %s\n", cwd);   
-//     free(cwd);
-//     return (EXIT_SUCCESS);
-// }
-
-int main(int ac,char **av)
+int ft_cd_test(char **a_arg)
 {
-    if (chdir(av[1]) == -1)
+    char    *cwd;
+
+    if (!a_arg[1])
+        return (EXIT_FAILURE);
+    cwd = getcwd(NULL, 0);
+    if (!cwd)
+    {
+        perror("getcwd");
+        return (EXIT_FAILURE);
+    }
+    printf("Current working directory: %s\n", cwd);
+    if (chdir(a_arg[1]) == -1)
     {
         perror("chdir failed");
         return (EXIT_FAILURE);       
     }
+    cwd = getcwd(NULL, 0);
+    if (!cwd)
+    {
+        perror("getcwd");
+        return (EXIT_FAILURE);
+    }
+    printf("Current working directory: %s\n", cwd);   
+    free(cwd);
+    return (EXIT_SUCCESS);
+}
+
+int ft_cd(char **a_arg)
+{
+    if (!a_arg[1])
+        return (EXIT_FAILURE);
+    if (chdir(a_arg[1]) == -1)
+    {
+        perror("chdir failed");
+        return (EXIT_FAILURE);       
+    }
+    return (EXIT_SUCCESS);
+}
+
+int main(int ac,char **av)
+{
+    ft_cd_test(av);
     ac = 0;
     return (0);
 }

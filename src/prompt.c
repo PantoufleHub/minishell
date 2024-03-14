9e3066660e1ae0465da6ccb4b81ff4c9427db4a0
SaitoZK <aperron@c1r14s7.42lausanne.ch>
2024-03-14 15:52:45 +0100

    Prompt cd character limit

--- src/prompt.c
+++ src/prompt.c
@@ -5,13 +5,21 @@ char	*get_prompt(void)
 	char	*prompt;
 	char	*cwd;
 	char	*cwd_np;
+	size_t	lim;
 
 	cwd = ft_calloc(4096, sizeof(char));
 	getcwd(cwd, 4096);
 	cwd_np = ft_strrchr(cwd, '/') + 1;
-	// CWD DOESNT UPDATE BY ITSELF!!
+	lim = 20;
+	if (ft_strlen(cwd_np) >= lim)
+	{
+		cwd_np[lim - 3] = '.';
+		cwd_np[lim - 2] = '.';
+		cwd_np[lim - 1] = '.';
+		cwd_np[lim] = 0;
+	}
 	prompt = strings_join((char *[]){RED, "@Swag"YEL"Shell:"GRN, getenv("USER"),
-		" "CYN, cwd_np, BLU"/: ", MAG, NULL});
+			" "CYN, cwd_np, BLU"/: ", MAG, NULL});
 	free(cwd);
 	return (prompt);
 }
